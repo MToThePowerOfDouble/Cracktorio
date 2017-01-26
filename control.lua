fa33e2be24887e775876d197f28fd3ec450dfe6c
Nexela <nexela@gmail.com>
2017-01-26 00:25:01 -0500

    Change script.on to point to functions Add init script Move global to init script

--- control.lua
+++ control.lua
@@ -1,24 +1,26 @@
-require "util"
+local function on_trigger_created_entity(event)
+    game.print("the trigger is working!")
+    global.isTimerOn = true
+    global.now = game.tick
+    global.time = game.tick
+    local player = game.players[1]
+    player.running_speed = .5
+end
+script.on_event(defines.events.on_trigger_created_entity, on_trigger_created_entity)
 
-global.isTimerOn = false
+local function on_tick(event)
+    if global.isTimerOn == true then
+        global.now = global.now + 1
+        if global.now == global.time + 100 then
+            game.print("the timer works after some ticks")
+            game.players[event.player.index].running_speed = 0.15
+            global.isTimerOn = false
+        end
+    end
+end
+script.on_event(defines.events.on_tick, on_tick)
 
- script.on_event(defines.events.on_trigger_created_entity, function(event)
-	 game.print("the trigger is working!")
-	 global.isTimerOn = true
-	 global.now = game.tick
-	 global.time = game.tick
-	 local player = game.players[1]
-	 player.running_speed = .5
-	 
- end)
-
- script.on_event(defines.events.on_tick, function(event)
-	if global.isTimerOn == true then
-		global.now = global.now + 1
-			if global.now == global.time + 100 then
-				game.print("the timer works after some ticks")
-				game.players[event.player.index].running_speed = 0.15
-				global.isTimerOn = false
-			end
-		end
- end)
+local function on_init()
+    global.isTimerOn = false
+end
+script.on_init(on_init)
