e4722094f8a26c8d68c2024c29475fa9ef800655
Nexela <nexela@gmail.com>
2017-01-26 23:37:23 -0500

    "Better logic"

--- control.lua
+++ control.lua
@@ -1,16 +1,47 @@
 local action = {}
+
+function action.nothing()
+end
+
 function action.remove_speed(data)
     local player, pdata = game.players[data.player_index], global.player_data[data.player_index]
     --Since we added player in a previous tick we need to make sure the player is still valid.
     if player and player.valid and player.character then
-		--change the modifier back only if it hasn't changes since we changed it earlier.
-		local cur_speed = player.character_running_speed_modifier
-		local cur_mining = player.character_mining_speed_modifier
-		--current issue -----2x modifier won't reset value
-		player.character_running_speed_modifier = data.modifier_new == cur_speed and data.modifier_old or cur_speed
-		player.character_mining_speed_modifier = data.mining_new == cur_mining and data.mining_old or cur_mining
-		player.print("the drugs have worn off")
+        --change the modifier back only if it hasn't changes since we changed it earlier.
+        local cur_speed = player.character_running_speed_modifier
+        local cur_mining = player.character_mining_speed_modifier
+        --current issue -----2x modifier won't reset value
+        player.character_running_speed_modifier = data.modifier_new == cur_speed and data.modifier_old or cur_speed
+        player.character_mining_speed_modifier = data.mining_new == cur_mining and data.mining_old or cur_mining
+        player.print("the drugs have worn off")
     end
+end
+
+function action.add_speed(player, tick)
+    --Add everything we will need to a data table
+    local data = {}
+    data.expires = tick + 240 --4 seconds from now
+    data.action = "remove_speed"
+    data.player_index = player.index
+    data.modifier_old = player.character_running_speed_modifier
+    data.modifier_new = player.character_running_speed_modifier + 0.5
+    player.character_running_speed_modifier = data.modifier_new
+    return data
+end
+
+function action.teleport(player)
+    local data = {}
+    local plx = player.position.x
+    local ply = player.position.y
+    --good and bad, look at surface.find_non_colliding_position
+    local location = {math.random(plx,50),math.random(ply, 50)}
+    player.teleport(location)
+    return data
+end
+
+function action.mining_speed(player)
+    data.mining_old = player.character_mining_speed_modifier
+    data.mining_new = player.character_mining_speed_modifier + 0.5
 end
 
 local function on_trigger_created_entity(event)
@@ -19,36 +50,22 @@ local function on_trigger_created_entity(event)
     local player = game.players[1]
     --Only affect players with characters
     if player and player.character then
-        --Add everything we will need to a data table
         local data = {}
-        --When does this effect expire
-        data.expires = event.tick + 240 --4 seconds from now
-        --What are we changing
-        data.action = "remove_speed"
-        data.player_index = player.index
-        data.modifier_old = player.character_running_speed_modifier
-		data.modifier_new = player.character_running_speed_modifier + 0.5
-		data.mining_old = player.character_mining_speed_modifier
-		data.mining_new = player.character_mining_speed_modifier + 0.5
-		--Change the current modifier value to the new value
-		--Determines what drug is being used
-		local player_stack = player.cursor_stack.name
-		if player_stack == "Caffeine-Capsule" then
-			player.character_running_speed_modifier = data.modifier_new
-		end
-		if player_stack == "Nicotine-Capsule" then
-			--player.character_mining_speed_modifier = data.mining_new
-			local plx = player.position.x
-			local ply = player.position.y
-			local location = {math.random(plx,50),math.random(ply, 50)}
-			player.teleport(location)
-		end
-			--Get or create a table of queues at position [expires]
-			global.tick_queue[data.expires] = global.tick_queue[data.expires] or {}
-        --Make local refrence to our queue
-        local queue = global.tick_queue[data.expires]
-        --Insert a new queue into table at expires tick position
-        queue[#queue+1] = data
+        local stack_name = player.cursor_stack and player.cursor_stack.valid_for_read and player.cursor_stack.name
+        if stack_name == "Caffeine-Capsule" then
+            data = action.add_speed(player, event.tick)
+        elseif stack_name == "Nicotine-Capsule" then
+            data = action.teleport(player)
+        end
+        --Only add to queue if it expires.
+        if data.expires then
+            --Get or create a table of queues at position [expires]
+            global.tick_queue[data.expires] = global.tick_queue[data.expires] or {}
+            --Make local refrence to our queue
+            local queue = global.tick_queue[data.expires]
+            --Insert a new queue into table at expires tick position
+            queue[#queue+1] = data
+        end
     end
 end
 script.on_event(defines.events.on_trigger_created_entity, on_trigger_created_entity)
